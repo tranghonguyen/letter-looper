5554cbd86297dafbbd1f63b3c9dbbc67dc51fe0b
Trang Ho-Nguyen <hong0066@algonquinlive.com>
2017-01-30 22:05:50 -0500

    Create looper.js

---
+++ js/looper.js
@@ -0,0 +1,12 @@
+var letter, i, k;
+while (!letter) {
+    letter = prompt("Enter a character!");
+}
+for (i = 0; i < 10; i+= 1) {
+    for (k = 0; k < i; k+= 1)
+    document.write(letter);{   
+    }
+    document.write("<br>");  
+}
+
+    
